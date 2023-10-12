3367842785f542039cd3ca49597e11c7685faf84
MathNerd28 <xander.bhalla@gmail.com>
2023-10-12 11:11:20 -0400

    Integrate DTLib into template (CI build will fail)

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,9 +1,11 @@
 package frc.robot;
 
+import org.victorrobotics.dtlib.DTRobot;
+
 public final class Main {
   private Main() {}
 
   public static void main(String... args) {
-
+    DTRobot.runRobot(Robot::new);
   }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,31 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-public class Robot {
+import org.victorrobotics.dtlib.DTRobot;
+import org.victorrobotics.dtlib.command.DTCommand;
+import org.victorrobotics.dtlib.command.DTNullCommand;
 
+public final class Robot extends DTRobot {
+  public Robot() {}
+
+  @Override
+  protected void init() {}
+
+  @Override
+  protected void simulationInit() {}
+
+  @Override
+  protected void bindCommands() {}
+
+  @Override
+  protected void periodic() {}
+
+  @Override
+  protected DTCommand getAutoCommand() {
+    return new DTNullCommand();
+  }
+
+  @Override
+  protected DTCommand getSelfTestCommand() {
+    return new DTNullCommand();
+  }
 }
